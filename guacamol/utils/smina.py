f3843ce17e6b152b16d74a84fe00b9be9588ee5c
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-07-17 22:22:59 +0200

    Add typing and remove type info from the documentation

--- guacamol/utils/smina.py
+++ guacamol/utils/smina.py
@@ -10,7 +10,9 @@ import logging
 import os
 import subprocess
 import tempfile
+from typing import Union, List, Tuple, Optional
 
+import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
@@ -35,7 +37,7 @@ proteins = {
 }
 
 
-def _embed_rdkit_molecule(molecule, seed: int, silent: bool = True):
+def _embed_rdkit_molecule(molecule: Chem.Mol, seed: int, silent: bool = True) -> Optional[Chem.Mol]:
     """
     Embeds RDKit molecule in place.
 
@@ -45,9 +47,8 @@ def _embed_rdkit_molecule(molecule, seed: int, silent: bool = True):
 
     Args:
         molecule: RDKit molecule
-        seed (int): Seed used for embedding
-        silent (bool): If False, exception is thrown when embedding fails.
-                          Otherwise, None is returned.
+        seed: Seed used for embedding
+        silent: If False, exception is thrown when embedding fails. Otherwise, None is returned.
 
     Returns:
         Embedded RDKit molecule if embedding succeeds.
@@ -72,7 +73,7 @@ def _embed_rdkit_molecule(molecule, seed: int, silent: bool = True):
     return molecule
 
 
-def _optimize_rdkit_molecule(molecule, silent: bool = False):
+def _optimize_rdkit_molecule(molecule: Chem.Mol, silent: bool = False) -> Optional[Chem.Mol]:
     """
     Optimizes the structure of RDKit molecule in place.
 
@@ -82,8 +83,7 @@ def _optimize_rdkit_molecule(molecule, silent: bool = False):
 
     Args:
         molecule: RDKit molecule
-        silent (bool): If False, exception is thrown when optimization fails.
-                          Otherwise, None is returned.
+        silent: If False, exception is thrown when optimization fails. Otherwise, None is returned.
 
     Returns:
         Embedded RDKit molecule if optimization succeeds.
@@ -108,16 +108,15 @@ def _optimize_rdkit_molecule(molecule, silent: bool = False):
     return molecule
 
 
-def _to_mol2_file(smiles: str, output_filename: str, seed: int = 0, silent: bool = False):
+def _to_mol2_file(smiles: str, output_filename: str, seed: int = 0, silent: bool = False) -> Optional[str]:
     """
     Converts a SMILES string to mol2 file.
 
     Args:
-        smiles (str): SMILES string of the molecule
-        output_filename (str): Path of the file the converted molecule will be saved to
-        seed (int): Seed used during optimizing and embedding molecule
-        silent (bool): If False, exception is thrown when conversion fails.
-                          Otherwise, None is returned.
+        smiles: SMILES string of the molecule
+        output_filename: Path of the file the converted molecule will be saved to
+        seed: Seed used during optimizing and embedding molecule
+        silent: If False, exception is thrown when conversion fails. Otherwise, None is returned.
 
     Returns:
         Output filename if the conversion succeeds, else None if silent is True.
@@ -148,7 +147,7 @@ def _to_mol2_file(smiles: str, output_filename: str, seed: int = 0, silent: bool
     return output_filename
 
 
-def _exec_docking(command: str, timeout: int = None):
+def _exec_docking(command: str, timeout: int = None) -> Tuple[List[str], List[str], int]:
     try:
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, timeout=timeout)
         out, err, return_code = str(result.stdout, 'utf-8').split('\n'), str(result.stderr, 'utf-8').split(
@@ -158,7 +157,7 @@ def _exec_docking(command: str, timeout: int = None):
         raise InvalidMolecule('Docking timeout')
 
 
-def _parse_smina_result(docked_ligand_fname, smina_stdout):
+def _parse_smina_result(docked_ligand_fname: str, smina_stdout: List[str]) -> List[float]:
     assert docked_ligand_fname.endswith('mol2')
 
     # WARNING: A Very hacky way to get score table
@@ -169,23 +168,24 @@ def _parse_smina_result(docked_ligand_fname, smina_stdout):
     return scores
 
 
-def smina_dock_ligand(smiles, receptor, pocket_center=None, pocket_range=25, exhaustiveness=16, seed=0,
-                      timeout=600, n_cpu=8):
+def smina_dock_ligand(smiles: str, receptor: str, pocket_center: Union[List, np.array, Tuple] = None,
+                      pocket_range: Union[int, List[int], np.array, Tuple[int]] = 25, exhaustiveness: int = 16,
+                      seed: int = 0, timeout: int = 600, n_cpu: int = 8) -> float:
     """
     Docks the passed molecule
 
     Args:
-        smiles (str): Ligand in SMILES format
-        receptor (str): Path to receptor in pdbqt or mol2 format
-        pocket_center (numpy.arrray): x, y, z coordinates of docking center. Must be of shape (3, ).
-        pocket_range (list, int): How far from the center are we trying to dock. If list is passed it must be of size 3.
-        exhaustiveness (int): Best practice is to bump to 16 (as compared to Vina 8s)
-        seed (int): Random seed passed to the smina simulator
-        timeout (int): Maximum waiting time in seconds
-        n_cpu (int): How many cpus to use
+        smiles: Ligand in SMILES format
+        receptor: Path to receptor in pdbqt or mol2 format
+        pocket_center: x, y, z coordinates of docking center
+        pocket_range: How far from the center are we trying to dock. If list is passed it must be of size 3.
+        exhaustiveness: Best practice is to bump to 16 (as compared to Vina 8s)
+        seed: Random seed passed to the smina simulator
+        timeout: Maximum waiting time in seconds
+        n_cpu: How many cpus to use
 
     Returns:
-        float: Docking score of the ligand
+        Docking score of the ligand
     """
     if type(pocket_range) is int:
         pocket_range = [pocket_range] * 3
